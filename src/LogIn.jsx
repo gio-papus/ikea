5153932c38f112d31f4f92691c0bf9e934e0d75c
Giorgi Meshveliani <xgorgaslax@gmail.com>
2024-02-11 18:57:24 +0100

    log-in-design-done

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,20 +1,23 @@
 import "./Login.css"
+import logo from "./images/Ikea_logo.svg.png"
 
 function LogIn(){
 return(
+    <>
     <div className="log-in-div">
-
+<img src={logo} alt="no img" className="logo"/>
             <form className="log-in-formulary">
                 <input className="log-in-user-name" type="text" placeholder="User Name"/>
                 
                 <br/>
 
-                <input className="log-in-pasword" type="pasword" placeholder="Password"/>
+                <input className="log-in-password" type="pasword" placeholder="Password"/>
             </form>
 
 
             <button className="log-in-button">Enter</button>
     </div>
+    </>
 )
 }
 
